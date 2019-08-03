f6ca5f8467d44a853f4f041584d2da3f62704a6e
artnum <etienne@artisan-numerique.ch>
2019-08-03 18:29:40 +0200

    Add more test on memory allocation

--- tests/mem-val.c
+++ tests/mem-val.c
@@ -1,13 +1,18 @@
 #include "mem.h"
+#include <stdlib.h>
+#include <stdio.h>
+#define PTRS_COUNT 500
 
 int main(int argc, char ** argv) {
+  int i = 0;
+  int j = 0;
   void * ptr = mem_alloc(10);
   void * ptr2 = mem_alloc(10);
   void * ptr3 = mem_alloc(10);
   void * ptr4 = NULL;
   void * ptr5 = NULL;
-
-
+  void * ptrs[PTRS_COUNT] = { NULL };
+  
   ptr5 = mem_cpy(ptr);
   ptr4 = mem_cpy(ptr2);
   mem_free(ptr);
@@ -27,6 +32,32 @@ int main(int argc, char ** argv) {
   mem_free(ptr3);
   mem_free(ptr4);
   mem_free(ptr5);
-  
+
+  for (i = 0; i < 200; i++) {
+    ptrs[i] = mem_alloc(100 * sizeof(int));
+    for (j = 0; j < 100; j++) {
+      ((int *)ptrs[i])[j] = rand();
+    }
+  }
+
+  for (i = 0; i < 200; i++) {
+    ptrs[i + 200] = mem_cpy(ptrs[i]);
+  }
+  for (i = 0; i < PTRS_COUNT; i++) {
+    mem_free(ptrs[i]);
+  }
+
+  ptr = mem_alloc(100 * sizeof(int));
+  for (j = 0; j < 100; j++) {
+    *((int *)(ptr + j)) = rand();    
+  }
+  for (i = 0; i < PTRS_COUNT; i++) {
+    ptrs[i] = mem_cpy(ptr);
+  }
+  for(i = 0; i < PTRS_COUNT; i++) {
+    mem_free(ptrs[i]);
+  }
+
+  mem_free(ptr);
   return 0;
 }
